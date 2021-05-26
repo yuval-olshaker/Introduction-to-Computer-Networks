f4f2cbec64b86c0e0fead08c662ee7aaf3fb1958
yuvalol <yuvalolshaker@gmail.com>
2021-05-26 14:51:03 +0300

    create method

--- ex3.cpp
+++ ex3.cpp
@@ -5,12 +5,47 @@
 #include <vector>
 using namespace std;
 
+
+vector<double> get_times(double T, double lambda){
+    vector <double> as;
+    a0 = 0.0; /* a convention */
+    as.push_back(a0);
+    while(as[as.size()-1] < T) {
+        as.push_back(as[as.size()-1] + Exponential(1 / λ));
+    }
+    return as;
+}
+
 int main (int argc, char *argv[]) {
-    double T = std::stod(argv[0]);
-    double lambda = std::stod(argv[1]);
-    double miu = std::stod(argv[2]);
-    vector<double> g1;
+    double T = stod(argv[0]);
+    double lambda = stod(argv[1]);
+    double miu = stod(argv[2]);
+    vector<double> ps;
     for (int i = 3 ; i < argc; i++){
-        g1.push_back(std::stod(argv[i]));
+        ps.push_back(stod(argv[i]));
+    }
+
+    vector<double> arrivel_times = get_times(double T, double lambda);
+    vector<double> queue;
+    vector<pair<int,double>> amounts;
+    X = 0;
+    Y = 0;
+    for (int i = 0 ; i < arrivel_times.size() ; i++){
+        amounts.push_back(make_pair(queue.size(),arrivel_times[i]));
+        pi = ps[queue.size()];
+        if ((double) rand()/RAND_MAX > pi){
+            X++;
+            continue;
+        }
+        Y++;
+        queue.push_back(arrivel_times[i]);
+        amounts.push_back(make_pair(queue.size(),arrivel_times[i]))
+        // moving out from queue
+        if (1 > 0){
+            int a = 5;
+        }
+
     }
 }
+
+
