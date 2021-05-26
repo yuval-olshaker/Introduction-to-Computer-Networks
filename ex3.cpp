6499eee51fa80d6188e0c5c21e6ec82d9f79b641
yuvalol <yuvalolshaker@gmail.com>
2021-05-26 23:32:49 +0300

    finish ex

--- ex3.cpp
+++ ex3.cpp
@@ -8,6 +8,15 @@
 
 using namespace std;
 
+double calc_mean(vector<double> vec){
+    double sumTotal = 0;
+    for(double service_time : vec){
+        // not sure what to put here basically.
+        sumTotal += service_time;
+    }
+    return sumTotal / vec.size();
+}
+
 double zero_to_one_random(){
     return (double) rand()/RAND_MAX;
 }
@@ -18,7 +27,7 @@ double distribution(double param){
 
 vector<double> get_times(double T, double lambda){
     vector <double> as;
-    double a0 = 0.0; /* a convention */
+    double a0 = 0.0;
     as.emplace_back(a0);
     while(as[as.size()-1] < T) {
         as.emplace_back(as[as.size()-1] + distribution(lambda));
@@ -28,6 +37,7 @@ vector<double> get_times(double T, double lambda){
 
 int main (int argc, char *argv[]) {
     srand (time(nullptr));
+    double T_tag;
     double T = stod(argv[1], nullptr);
     double lambda = stod(argv[2], nullptr);
     double miu = stod(argv[3], nullptr);
@@ -35,11 +45,16 @@ int main (int argc, char *argv[]) {
     for (int i = 4 ; i < argc; i++){
         ps.emplace_back(stod(argv[i], nullptr));
     }
-
+    vector<double> service_times;
+    double dist;
+    vector<double> waiting_times;
+    double start_time;
     vector<double> arrivel_times = get_times(T, lambda);
     vector<double> queue;
     vector<double> finish_times;
     vector<pair<int,double>> amounts;
+    vector<double> inserted_times;
+    double last_time = -1;
     int X = 0;
     int Y = 0;
     for (int i = 0 ; i < arrivel_times.size() ; i++){
@@ -50,10 +65,23 @@ int main (int argc, char *argv[]) {
         } else {
             Y++;
             queue.emplace_back(arrivel_times[i]);
+            if (last_time == -1){
+                last_time = arrivel_times[i];
+            } else{
+                inserted_times.emplace_back(arrivel_times[i] - last_time);
+                last_time = arrivel_times[i];
+            }
             if (finish_times.empty()){
-                finish_times.emplace_back(arrivel_times[i] + distribution(miu));
+                dist = distribution(miu);
+                service_times.emplace_back(dist);
+                waiting_times.emplace_back(0);
+                finish_times.emplace_back(arrivel_times[i] + dist);
             } else {
-                finish_times.emplace_back(finish_times[finish_times.size() - 1] + distribution(miu));
+                dist = distribution(miu);
+                service_times.emplace_back(dist);
+                start_time = finish_times[finish_times.size() - 1];
+                waiting_times.emplace_back(start_time - arrivel_times[i]);
+                finish_times.emplace_back(start_time + dist);
             }
 
             amounts.emplace_back(make_pair(queue.size(),arrivel_times[i]));
@@ -61,6 +89,7 @@ int main (int argc, char *argv[]) {
         // deal with finish packets
         if (i+1 == arrivel_times.size()){ // if last packet we finish with all
             int len = finish_times.size();
+            T_tag = finish_times[len-1];
             for (int k = 0; k < len; k++) {
                 double time = finish_times[0];
                 queue.erase(queue.begin());
@@ -85,8 +114,31 @@ int main (int argc, char *argv[]) {
             }
         }
     }
-    cout << "X" << X << endl;
-    cout << "Y" << Y << endl;
+    cout << X << " ";
+    cout << Y << " ";
+    cout << T_tag << " ";
+    vector<double> times;
+    for (int i = 0 ; i < argc - 4; i++){
+        times.emplace_back(0);
+    }
+    pair<int,double> last = amounts[0];
+    pair<int,double> next;
+    for (int i = 1; i < amounts.size() ; i++) {
+        next = amounts[i];
+        times[last.first] += (next.second - last.second);
+        last = next;
+    }
+    for (double time : times) {
+        cout << time << " ";
+    }
+    for (double time : times) {
+        cout << (time / T_tag) << " ";
+    }
+
+
+    cout << calc_mean(waiting_times) << " ";
+    cout << calc_mean(service_times) << " ";
+    cout << (1 / calc_mean(inserted_times)) << " ";
 }
 
 
